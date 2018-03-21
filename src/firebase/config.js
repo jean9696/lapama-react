9fe0619468fba1760e392a10b55f45793eb37a5a
Jean Dessane <jean.dessane@gmail.com>
2018-03-21 09:10:30 +0000

    conf

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-// no ENV variables available on my hosting :(
-export default {
-  apiKey: 'AIzaSyB1vnhNUMd71eejrSkLmg254rBwfCiTWTk',
-  authDomain: 'maximal-relic-150922.firebaseapp.com',
-  databaseURL: 'https://maximal-relic-150922.firebaseio.com',
-  projectId: 'maximal-relic-150922',
-  storageBucket: 'maximal-relic-150922.appspot.com',
-  messagingSenderId: '318528776798',
-};
